0a95d656e79068462fe61bf0b7f55cba2c39cd4b
João Vitor Espig <jotinha1300@gmail.com>
2023-01-19 21:05:06 -0300

    added SpaceCenter type

--- src/types.hpp
+++ src/types.hpp
@@ -4,6 +4,7 @@
 #include <krpc/services/space_center.hpp>
 
 
+typedef krpc::services::SpaceCenter SpaceCenter;
 typedef krpc::services::SpaceCenter::SASMode SASMode;
 typedef krpc::services::SpaceCenter::SpeedMode SpeedMode;
 typedef krpc::services::SpaceCenter::Vessel Vessel;
