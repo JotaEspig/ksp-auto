c2abceac358d5b2063d5c95c318a855a59bda6e9
João Vitor Espig <jotinha1300@gmail.com>
2023-02-06 12:04:13 -0300

    finished the 2 left functions

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,21 +1,47 @@
 #include "menu.hpp"
 
+#include <iostream>
+
 #include "suicideburn.hpp"
+#include "launchorbit.hpp"
+#include "maneuver.hpp"
+#include "types.hpp"
 
 
-static void run_suicide_burn(Vessel vessel) {
+static void run_suicide_burn(SpaceCenter sc) {
+    Vessel vessel = sc.active_vessel();
     suicide_burn(vessel);
 }
 
-static void run_launch_into_orbit(Vessel vessel) {
+static void run_launch_into_orbit(SpaceCenter sc) {
+    int target_alt, turn_start_alt, turn_end_alt;
 
+    Vessel vessel = sc.active_vessel();
+    
+    std::cout << "Target altitude: ";
+    std::cin >> target_alt;
+    std::cout << "Altitude at the start of the gravitational turn: ";
+    std::cin >> turn_start_alt;
+    std::cout << "Altitude at the end of the gravitational turn: ";
+    std::cin >> turn_end_alt;
+
+    launch_into_orbit(sc, vessel, target_alt, turn_start_alt, turn_end_alt);
 }
 
-static void run_do_maneuver(Vessel vessel) {
+static void run_do_maneuver(SpaceCenter sc) {
+    int choice;
+    Node node;
 
+    Vessel vessel = sc.active_vessel();
+
+    std::cout << "Maneuver you want to do automatically (starting with 0): ";
+    std::cin >> choice;
+    
+    node = vessel.control().nodes().at(choice);
+    do_maneuver(vessel, node);
 }
 
-std::map<std::string, std::function<void(Vessel)>> menu::commands = {
+std::map<std::string, std::function<void(SpaceCenter)>> menu::commands = {
     {"suicideburn", run_suicide_burn},
     {"launch", run_launch_into_orbit},
     {"maneuver", run_do_maneuver}
