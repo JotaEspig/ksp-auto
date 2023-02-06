b3e1f63638ae478f97bf6b8faf3cbeea32af551c
João Vitor Espig <jotinha1300@gmail.com>
2023-02-06 12:03:08 -0300

    using SpaceCenter as parameter instead of vessel

--- src/menu.hpp
+++ src/menu.hpp
@@ -7,10 +7,10 @@
 #include "types.hpp"
 
 
-static void run_suicide_burn(Vessel vessel);
-static void run_launch_into_orbit(Vessel vessel);
-static void run_do_maneuver(Vessel vessel);
+static void run_suicide_burn(SpaceCenter sc);
+static void run_launch_into_orbit(SpaceCenter sc);
+static void run_do_maneuver(SpaceCenter sc);
 
 namespace menu {
-    extern std::map<std::string, std::function<void(Vessel)>> commands;
+    extern std::map<std::string, std::function<void(SpaceCenter)>> commands;
 }
