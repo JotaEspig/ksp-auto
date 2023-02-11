6b14359015b2dcddee79885f092e15e4b068b2a5
João Vitor Espig <jotinha1300@gmail.com>
2023-02-10 21:02:48 -0300

    using thrust stream to improve accuracy in suicideburn

--- src/suicideburn.cpp
+++ src/suicideburn.cpp
@@ -9,10 +9,10 @@
 void suicide_burn(Vessel vessel) {
     float twr, height_sb, thrust_to_final_speed;
 
-    float thrust = vessel.available_thrust();
     float body_gravity = vessel.orbit().body().surface_gravity();
     auto body_refframe = vessel.orbit().body().reference_frame();
 
+    auto thrust_stream = vessel.available_thrust_stream();
     auto mass_stream = vessel.mass_stream();
     auto throttle_stream = vessel.control().throttle_stream();
     auto velocity_stream = vessel.flight(body_refframe).velocity_stream();
@@ -21,12 +21,14 @@ void suicide_burn(Vessel vessel) {
     auto h_speed_stream = vessel.flight(body_refframe).
         horizontal_speed_stream();
     auto height_stream = vessel.flight(body_refframe).
-        bedrock_altitude_stream();
+        surface_altitude_stream();
 
-    if (thrust == 0) {
+    if (thrust_stream() == 0) {
         std::cout << "No available thrust!\n";
         exit(-1);
     }
+
+    int safe_margin = 20;
 
     vessel.auto_pilot().engage();
     vessel.auto_pilot().set_reference_frame(body_refframe);
@@ -37,7 +39,7 @@ void suicide_burn(Vessel vessel) {
     std::cout << "Removing horizontal speed\n";
     vessel.control().set_speed_mode(SpeedMode::surface);
     while (h_speed_stream() > 10) {
-        twr = thrust / (body_gravity * mass_stream());
+        twr = thrust_stream() / (body_gravity * mass_stream());
         height_sb = get_height(twr, v_speed_stream(), body_gravity);
         set_retrograde_direction(vessel, velocity_stream());
         if (height_stream() > height_sb + 2000) continue;
@@ -49,8 +51,8 @@ void suicide_burn(Vessel vessel) {
     vessel.control().set_speed_mode(SpeedMode::surface);
     vessel.control().set_gear(true);
     while (speed_stream() > FINAL_SPEED) {
-        twr = thrust / (body_gravity * mass_stream());
-        height_sb = get_height(twr, speed_stream(), body_gravity) + 20; // 20 is a safe margin
+        twr = thrust_stream() / (body_gravity * mass_stream());
+        height_sb = get_height(twr, speed_stream(), body_gravity) + safe_margin;
         set_retrograde_direction(vessel, velocity_stream());
         if (height_sb < height_stream()) {
             if (throttle_stream() > 0) vessel.control().set_throttle(0);
@@ -61,7 +63,7 @@ void suicide_burn(Vessel vessel) {
 
     std::cout << "Final stage\n";
     while(height_stream() > 5) {
-        twr = thrust / (body_gravity * mass_stream());
+        twr = thrust_stream() / (body_gravity * mass_stream());
         thrust_to_final_speed = 1/twr;
         if (throttle_stream() == 1)
             vessel.control().set_throttle(thrust_to_final_speed);
